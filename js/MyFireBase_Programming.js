03ea7610fc563ba78913248f1ff68fe347198994
Spartan334 <ianmina0322@gmail.com>
2022-12-24 22:50:58 +0800

    完成firebase下載檔案+聊天室

--- js/MyFireBase_Programming.js
+++ js/MyFireBase_Programming.js
@@ -15,44 +15,153 @@ $(document).ready(function () {
 
     let db = firebase.firestore();
     let dbStorage = firebase.storage();
-    
+
+    // 檔案下載區
 
     $(".FileBtn").click(function (e) { 
         e.preventDefault();
         
+        $(this).html(`<span class="spinner-border spinner-border-sm"></span>`);
+
         let Path = $(this).attr("id");
         let FullPath = "Computer_Progrmming/"+Path+".zip";
 
+        
         dbStorage.ref(FullPath).getDownloadURL()
         .then(function(url) {
 
-            fetch(url)
-            .then(res => res.blob())
-            .then(blob => {
-                let a = document.createElement("a");
-                let url = window.URL.createObjectURL(blob);
-                a.href = url;
-                a.download = name;
+            fetch(url ,{
 
-                // Firefox 需要將 JS 建立出的 element appendChild 到 DOM 上才可以 work
-                a.style.display = "none";
-                document.body.appendChild(a);
+                mode:"no-cors"
 
-                a.click();
+            }).then(function(){
+                
+            let a = document.createElement("a");
+            
+            // 用 createObjectURL 將 blob 創建一個我們本地端瀏覽器中的 URL 對象
+            a.href = url;
+            a.download = Path;
+    
+            // Firefox 需要將 JS 建立出的 element appendChild 到 DOM 上才可以 work
+            a.style.display = "none";
+            document.body.appendChild(a);
+    
+            a.click();
+    
+            // 刪除多餘的 DOM 與 釋放記憶體
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+           
+         });
+          
+              
+         })
+         .then(function(){
 
-                // 刪除多餘的 DOM 與 釋放記憶體
-                document.body.removeChild(a);
-                window.URL.revokeObjectURL(url);
-      });
+            $(".FileBtn").html(`<img src="./img/47504268.jpg" height="100%" width="100%" alt="">`);
+         })
+         .catch(function(error){
+
+
+            $(".FileBtn").html(`<img src="./img/47504268.jpg" height="100%" width="100%" alt="">`);
+            alert("No File");
+
 
         })
-        .catch(function(error){
 
-            alert("No File");
+    });
+
+
+
+    // 聊天室後臺擷取
+    let ChatRoomMessage = db.collection("chatRoom").doc("Room1");
+    let messageCollection = ChatRoomMessage.collection("Message");
+    const $messageList = $("#message-list");
+    const $messageField = $("#message-field");
+
+    $messageField.keypress(function (e) { 
+
+        if(e.keyCode == 13){
+
+            SendData($messageField , messageCollection );
+        }
+
+    });
+
+
+    $("#SubmitBtn").click(function (e) { 
+        e.preventDefault();
+
+
+        SendData($messageField , messageCollection );
+
+    });
+
+    // 聊天室後臺擷取並顯示
+
+    let queryMessageCollectionRef = messageCollection.orderBy("SendTime","asc");
+
+    queryMessageCollectionRef.onSnapshot(function(querySnapshot){
+
+        $messageList.html("");
+
+        querySnapshot.forEach(doc => {
+            
+            let message = doc.data().message;
+            let mesTime = new Date(doc.data().SendTime);
+
+            let messageItem = `
+            <li class="message-item">
+                <img class ="UserImg" src="./img/47504268.jpg" >
+                <strong class="chat">${message}</strong>
+                <strong class="chatTime">${mesTime}</strong>
+
+            </li>
+            `
+
+            $messageList.append(messageItem);
+            
 
 
         });
 
+        $messageList[0].scrollTop = $messageList[0].scrollHeight;
+
     });
+   
+
+     // 聊天室滾動
+
+     
+
+
+
+
 
 });
+
+function SendData( M , FireBase ){
+
+    let message = M.val();
+    if(message[0] != " " && message != ""){
+        if(message.length >=34 ){
+            alert("屁話一堆! 字數過多");
+        }
+        else{
+            FireBase.add({
+
+        
+                SendTime: Date.now(),
+                message: message,
+        
+        
+            });
+        }
+      
+    }
+   
+
+    M.val('');
+
+
+}
